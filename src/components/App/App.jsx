483da74bd37dcafc33133906225db0692c2fee9b
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-13 12:23:32 -0500

    confirmed GET working in console log

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+
+
 function App() {
+  const [galleryItems, setGalleryItems] = useState([])
+
+  // GET path
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then( (response) => {
+      setGalleryItems(response.data)
+      console.log('in fetchGallery', galleryItems)
+    })
+    .catch(function (error) {
+      console.log('Error with GET:', error)
+    })
+  }
+
+  useEffect( () => {
+    fetchGallery()
+  }, [])
+
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <galleryItems list={galleryItems}/>
       </div>
     );
 }
