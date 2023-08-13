4c737d1b637c5f83bf3d9d9dce57721d84b775d6
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-13 11:44:52 -0500

    updated photo paths and description in gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/duluth-camping.jpg', description: 'Vinny and I at Gooseberry Falls for our yearly Duluth trip.', likes: 0 },
+    { id: 2, path: 'images/friends-halloween.jpg', description: 'Costume party at Up Down with friends', likes: 0 },
+    { id: 3, path: 'images/lucy-puppy.jpg', description: 'My dog Lucy, who is nearly always attached to my hip', likes: 0 },
+    { id: 4, path: 'images/pumpkin-carving.jpg', description: 'Carving pumpkins with my long time best friend Paige and her family.', likes: 0 },
+    { id: 5, path: 'images/vinny-baking.jpg', description: 'A constant fun activity in our home is baking. Vinny decorated our Thanksgiving pie to have crust "arms" and a whipcream face.', likes: 0 },
+    { id: 6, path: 'images/zombie-pubcrawl.jpg', description: 'My friend Courtney and I dressed as 50`s themed house wife & waitress zombies for Zombie pubcrawl', likes: 0 }
 ];
 
 module.exports = galleryItems;
